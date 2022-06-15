6cd2feab4f211ba6dca85658b898d46660d0abb0
beckernathan52 <beckernathan.9510@gmail.com>
2022-06-15 13:16:39 +0200

    add function like and dislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -11,7 +11,7 @@ const checkSauceValid = (req) => {
     const sauceMainPepper = sauceObject.mainPepper
     const sauceHeat = sauceObject.heat
     
-    if (!sauceName.trim() || !sauceManufacturer.trim() || !sauceDescription.trim() || !sauceMainPepper.trim() || !sauceHeat.trim()) {
+    if (!sauceName.trim() || !sauceManufacturer.trim() || !sauceDescription.trim() || !sauceMainPepper.trim() || !sauceHeat) {
         return false
     } 
     return true
@@ -21,8 +21,9 @@ const checkSauceValid = (req) => {
 const checkSauceExist = async (req) => {
     try {
         const sauceFound = await Sauce.findOne({ _id: req.params.id })
+        
         return sauceFound
-
+        
     } catch (error) {
         console.log(error)
     }
@@ -49,7 +50,7 @@ const createSauce = async (req, res, next) => {
     
     try {
         await sauce.save()
-        res.status(201).json({ message: 'Objet enregistré !'})
+        res.status(201).json({ message: 'Sauce enregistré !'})
     } catch (error) {
         res.status(500).json({ error: "Une erreur est survenue !" })
         console.log(error)
@@ -58,6 +59,7 @@ const createSauce = async (req, res, next) => {
 
 // Mise à jour d'une sauce existante
 const modifySauce = async (req, res, next) => {
+
     try {
         const sauceExist = await checkSauceExist(req)
         
@@ -65,12 +67,14 @@ const modifySauce = async (req, res, next) => {
         if (!sauceExist) {
             return res.status(404).json({ error: "La sauce est inexistante."})
         }
+
         // Vérifie si l'utilisateur est autorisé
         if (sauceExist.userId !== req.auth.userId) {
             return res.status(403).json({ error: "Vous n'êtes pas autorisé à effectuer cette opération."})
         }
+
         // Vérifie si l'ensemble des champs sont valide
-        if (!req.body.name.trim() || !req.body.manufacturer.trim() || !req.body.description.trim() || !req.body.mainPepper.trim() || !req.body.heat.trim()) {
+        if (!req.body.name.trim() || !req.body.manufacturer.trim() || !req.body.description.trim() || !req.body.mainPepper.trim() || !req.body.heat) {
             return res.status(422).json({message: "Requête incorrecte, des informations sont manquantes."})
         }
         
@@ -96,25 +100,22 @@ const modifySauce = async (req, res, next) => {
                 sauceObject = { ...req.body }
         }
     
-        // const sauceObject = req.file ? {
-        //     // Opération spread pour faire une copie de la requête
-        //     ...JSON.parse(req.body.sauce),
-        //     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body} 
-    
         try {
             await Sauce.updateOne({ _id: req.params.id, userId: req.auth.userId}, { ...sauceObject, _id: req.params.id })
 
-            res.status(200).json({ message: 'Objet modifié !'})
+            res.status(200).json({ message: 'Sauce modifié !'})
 
         } catch (error) {
-            res.status(400).json({ error })
-            console.log(error)
+            res.status(400).json({message: error.message })
+            console.log(error) 
         }
+
     } catch (error) {
         res.status(500).json({ error: "Une erreur est survenue !" })
-        console.log("ici", error)
+        console.log(error)
     }
 }
+
 
 // Suppression d'une sauce
 const deleteSauce = async (req, res, next) => {
@@ -129,11 +130,17 @@ const deleteSauce = async (req, res, next) => {
         if (sauceExist.userId !== req.auth.userId) {
             return res.status(403).json({ error: "Vous n'êtes pas autorisé à effectuer cette opération."})
         }
-    
+        
         try {
-            await Sauce.deleteOne({ _id: req.params.id })
+            const filename = sauceExist.imageUrl.split('/images/')[1]
+            fs.unlink(`images/${filename}`, error => {
+                if (error) throw error;
+                console.log('Image effacée !');
+            })
             
-            res.status(200).json({ message: 'Objet supprimé !'})
+            await Sauce.deleteOne({ _id: req.params.id })
+            res.status(200).json({ message: 'Sauce supprimé !'})
+            
         } catch (error) {
             res.status(400).json({ error })
             console.log(error)
@@ -144,14 +151,16 @@ const deleteSauce = async (req, res, next) => {
     }
 }
 
+
 // Récupération d'une sauce spécifique
 const getOneSauce = async (req, res, next) => {
+    
     try {
         const sauceExist = await checkSauceExist(req)
 
         // Vérifie si la sauce existe
         if (!sauceExist) {
-            res.status(404).json({ error: "Aucune sauce n'a été trouvé"})  
+            res.status(404).json({ error: "Aucune sauce n'a été trouvé."})  
         }
         res.status(200).json(sauceExist)
         
@@ -160,6 +169,7 @@ const getOneSauce = async (req, res, next) => {
         console.log(error)
     }
 }
+
 
 // Récupération de la liste de Sauces
 const getAllSauce = async (req, res, next) => {
@@ -177,4 +187,101 @@ const getAllSauce = async (req, res, next) => {
     }
 }
 
-export {createSauce, modifySauce, deleteSauce, getOneSauce, getAllSauce}
+
+// Like et Dislike
+const likeOrDislike = async (req, res, next) => {
+    
+    const userId = req.body.userId
+    const like = req.body.like
+
+    try{
+        // Vérifie si la sauce existe
+        const sauceExist = await checkSauceExist(req)
+
+        if (!sauceExist) {
+            res.status(404).json({ error: "Aucune sauce n'a été trouvé."})  
+        }
+        
+        // Like
+        if (like === 1) {
+            
+            // Si l'utilisateur like déjà la sauce
+            if (sauceExist.usersLiked.includes(userId)) {
+                return res.status(403).json({message: "Vous likez déjà la sauce !"})
+            }
+
+            // Si l'utilisateur tente de dislike la sauce alors qu'il avait like
+            if (sauceExist.usersDisliked.includes(userId)) {
+                return res.status(403).json({message: "Impossible de liké et disliké une même sauce."})
+            }
+
+            try {
+                const sauceLiked = await Sauce.updateOne({_id: req.params.id}, {$inc: { likes: 1 }, $push: { usersLiked: userId }})
+                res.status(200).json({message: 'Sauce likée !'})
+
+            } catch (error) {
+                console.log(error)
+                res.status(400).json({message: error.message})
+            }
+        }
+            
+        // Dislike
+        if (like === -1) {
+            
+            // Si l'utilisateur a like la sauce alors qu'il dislikait
+            if (sauceExist.usersLiked.includes(userId)) {
+                return res.status(403).json({message: "Impossible de liké et disliké une même sauce."})
+            }
+
+            // Si l'utilisateur dislike déjà la sauce
+            if (sauceExist.usersDisliked.includes(userId)) {
+                return res.status(403).json({message: "Sauce déjà dislikée !"})
+            }  
+
+            try {
+                const sauceDisliked = await Sauce.updateOne({_id: req.params.id}, {$inc: { dislikes: 1 }, $push: { usersDisliked: userId }})
+                res.status(200).json({message: 'Sauce dislikée !'})
+
+            } catch (error) {
+                console.log(error)
+                res.status(400).json({message: error.message})
+            }
+        }
+
+        // Annulation
+        if (like === 0) {
+
+            // Annulation de like
+            if (sauceExist.usersLiked.includes(userId)) {
+
+                try {
+                    const sauceLiked = await Sauce.updateOne({_id: req.params.id}, {$inc: { likes: -1 }, $pull: { usersLiked: userId }})
+                    res.status(200).json({message: 'Like annulé !'})
+                 
+                } catch (error) {
+                    res.status(400).json({message: error.message})
+                    console.log(error)
+                }
+            }
+
+            // Annulation de dislike
+            if (sauceExist.usersDisliked.includes(userId)) {
+
+                try {
+                    const sauceDisliked = await Sauce.updateOne({_id: req.params.id}, {$inc: { dislikes: -1 }, $pull: { usersDisliked: userId }})
+                    res.status(200).json({message: 'Dislike annulé !'})
+
+                } catch (error) {
+                    res.status(400).json({message: error.message})
+                    console.log(error)
+                }
+            }
+        }
+
+    } catch (error) {
+        res.status(404).json({message: error.message})
+        console.log(error)
+    }
+}
+
+export {createSauce, modifySauce, deleteSauce, getOneSauce, getAllSauce, likeOrDislike}
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,6 +1,6 @@
 // Importation des dépendances
 import { Router } from 'express'
-import { createSauce, deleteSauce, getAllSauce, getOneSauce, modifySauce } from '../controllers/sauce.js'
+import { createSauce, deleteSauce, getAllSauce, getOneSauce, modifySauce, likeOrDislike } from '../controllers/sauce.js'
 import { authentication } from '../middleware/auth.js'
 import { imgStorage } from '../middleware/multer-config.js'
 
@@ -13,5 +13,6 @@ routerSauces.get('/', authentication, getAllSauce)
 routerSauces.get('/:id', authentication, getOneSauce)
 routerSauces.put('/:id', authentication, imgStorage, modifySauce)
 routerSauces.delete('/:id', authentication, deleteSauce)
+routerSauces.post('/:id/like', authentication, likeOrDislike)
 
 export {routerSauces}
